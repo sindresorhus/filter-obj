4a0ca44c1c805d497c7e381e6b68157c1d13c0c1
ehmicky <ehmicky@users.noreply.github.com>
2022-06-03 11:32:05 +0200

    Add more tests (#13)

--- test.js
+++ test.js
@@ -1,19 +1,48 @@
 import test from 'ava';
 import filterObject from './index.js';
 
-test('function predicate', t => {
+test('function predicate returns a boolean', t => {
 	t.is(Object.keys(filterObject({foo: true, bar: false}, () => true)).length, 2);
 	t.is(Object.keys(filterObject({foo: true, bar: false}, () => false)).length, 0);
-	t.is(Object.keys(filterObject({foo: true, bar: false}, (key, value) => value === true)).length, 1);
+});
+
+test('function predicate passes the key as argument', t => {
+	t.true(filterObject({foo: true}, key => key === 'foo').foo);
+});
+
+test('function predicate passes the value as argument', t => {
+	t.is(filterObject({foo: 'test'}, (key, value) => value === 'test').foo, 'test');
+});
+
+test('function predicate passes the object as argument', t => {
+	t.true(filterObject({foo: true}, (key, value, object) => object.foo).foo);
 });
 
 test('array predicate', t => {
-	const filteredKeys = Object.keys(filterObject({foo: true, bar: false}, ['foo']));
-	t.is(filteredKeys[0], 'foo');
-	t.is(filteredKeys.length, 1);
+	t.deepEqual(Object.keys(filterObject({foo: true, bar: false}, ['foo'])), ['foo']);
+});
+
+test('symbol properties are omitted', t => {
+	const symbol = Symbol('test');
+	const input = {[symbol]: true};
+	t.is(filterObject(input, () => true)[symbol], undefined);
+});
+
+test('non-enumerable properties are omitted', t => {
+	const input = Object.defineProperty({}, 'test', {value: true, enumerable: false});
+	t.is(filterObject(input, () => true).test, undefined);
+});
+
+test('inherited properties are omitted', t => {
+	const Parent = class {
+		test() {}
+	};
+	const Child = class extends Parent {};
+	const input = new Child();
+	t.is(filterObject(input, () => true).test, undefined);
 });
 
 test('__proto__ keys', t => {
-	const input = {['__proto__']: {foo: true}};
+	const input = {__proto__: {foo: true}};
 	t.deepEqual(filterObject(input, () => true), input);
 });
