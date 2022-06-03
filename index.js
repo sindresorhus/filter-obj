6098d8db9e6616fbc493dd8e21764ed785db6590
ehmicky <ehmicky@users.noreply.github.com>
2022-06-03 11:37:37 +0200

    Improve performance of array predicates (#15)

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 export default function filterObject(object, predicate) {
 	const result = {};
-	const isArray = Array.isArray(predicate);
 
-	for (const [key, value] of Object.entries(object)) {
-		if (isArray ? predicate.includes(key) : predicate(key, value, object)) {
-			Object.defineProperty(result, key, {
-				value,
-				writable: true,
-				enumerable: true,
-				configurable: true,
-			});
+	if (Array.isArray(predicate)) {
+		const set = new Set(predicate);
+		for (const key of Object.keys(object)) {
+			if (set.has(key)) {
+				const value = object[key];
+				Object.defineProperty(result, key, {
+					value,
+					writable: true,
+					enumerable: true,
+					configurable: true,
+				});
+			}
+		}
+	} else {
+		for (const [key, value] of Object.entries(object)) {
+			if (predicate(key, value, object)) {
+				Object.defineProperty(result, key, {
+					value,
+					writable: true,
+					enumerable: true,
+					configurable: true,
+				});
+			}
 		}
 	}
 
