c4552a01e5919f9758d0cbe4080dbd4ae1b1efc0
ehmicky <ehmicky@gmail.com>
2022-07-25 18:22:33 +0200

    Rename `propSymbol` to `propertySymbol`

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,16 +1,16 @@
 import {expectType, expectError} from 'tsd';
 import {includeKeys, excludeKeys} from './index.js';
 
-const propSymbol = Symbol('test');
+const propertySymbol = Symbol('test');
 const object = {
 	foo: 'foo',
 	bar: 1,
-	[propSymbol]: true,
+	[propertySymbol]: true,
 };
 
 expectType<Partial<typeof object>>(
 	includeKeys(object, (key, value) => {
-		expectType<'foo' | 'bar' | typeof propSymbol>(key);
+		expectType<'foo' | 'bar' | typeof propertySymbol>(key);
 		expectType<string | number | boolean>(value);
 
 		return false;
@@ -20,13 +20,13 @@ expectError<typeof object>(
 	includeKeys(object, () => false),
 );
 expectType<{foo: string}>(includeKeys(object, ['foo']));
-expectType<{[propSymbol]: boolean}>(includeKeys(object, [propSymbol]));
+expectType<{[propertySymbol]: boolean}>(includeKeys(object, [propertySymbol]));
 expectError<typeof object>(includeKeys(object, ['foo']));
 expectError(includeKeys(object, ['baz']));
 
 expectType<Partial<typeof object>>(
 	excludeKeys(object, (key, value) => {
-		expectType<'foo' | 'bar' | typeof propSymbol>(key);
+		expectType<'foo' | 'bar' | typeof propertySymbol>(key);
 		expectType<string | number | boolean>(value);
 
 		return false;
@@ -35,7 +35,7 @@ expectType<Partial<typeof object>>(
 expectError<typeof object>(
 	excludeKeys(object, () => false),
 );
-expectType<{bar: number; [propSymbol]: boolean}>(excludeKeys(object, ['foo']));
-expectType<{foo: string; bar: number}>(excludeKeys(object, [propSymbol]));
+expectType<{bar: number; [propertySymbol]: boolean}>(excludeKeys(object, ['foo']));
+expectType<{foo: string; bar: number}>(excludeKeys(object, [propertySymbol]));
 expectError<typeof object>(excludeKeys(object, ['foo']));
 expectError(excludeKeys(object, ['baz']));
