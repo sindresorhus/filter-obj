c25176930834885f94886627d4e5122e652b95b1
Mathias Schreck <schreck.mathias@gmail.com>
2024-03-11 15:32:58 +0100

    Fix excludeKeys return type when passing in a union of objects
    The builtin `Omit` type doesn’t work correctly for union types. Using a
    [distributive conditional
    type](https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types)
    allows to apply the exclusion of the given key(s) for all members of the
    union.

--- index.test-d.ts
+++ index.test-d.ts
@@ -39,3 +39,7 @@ expectType<{bar: number; [propertySymbol]: boolean}>(excludeKeys(object, ['foo']
 expectType<{foo: string; bar: number}>(excludeKeys(object, [propertySymbol]));
 expectError<typeof object>(excludeKeys(object, ['foo']));
 expectError(excludeKeys(object, ['baz']));
+
+type UnionOfObjects = { type: 'foo'; foo: string } | { type: 'bar'; bar: number };
+const object2: UnionOfObjects = { type: 'foo', foo: 'test' } as UnionOfObjects;
+expectType<{ foo: string } | { bar: number }>(excludeKeys(object2, ['type']));
